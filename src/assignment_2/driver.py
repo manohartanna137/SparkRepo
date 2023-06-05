f39691b0c17c692b69c402b38bd52111d59e45f0
manohartanna137 <tanna.manohar@diggibyte.com>
2023-06-05 16:31:18 +0530

    This is a driver file for gh torrent logs data

--- src/assignment_2/driver.py
+++ src/assignment_2/driver.py
@@ -1,32 +1,29 @@
+
 from src.assignment_2.utils import *
+
 file_path = "../../resource/ghtorrent-logs.txt"
 
-print('hello')
-
-log_rdd = load_log_rdd(file_path)
+log_rdd = load_rdd(file_path)
 print(log_rdd)
 
 line_count = count_lines(log_rdd)
 print("Number of lines:", line_count)
 
-
 warning_count = count_warning_messages(log_rdd)
-print('Number of warnings:',warning_count)
-
+print('Number of warnings:', warning_count)
 
 api_client_lines = count_processed_repositories(log_rdd)
-print("Number of api client lines:",api_client_lines_count)
+print("Number of api client lines:", api_client_lines)
 
-
-most_requests_client = client_with_most_requests(api_client_lines)
-print("Client with most failed requests:", failed_rdd[0])
+api_client_lines = log_rdd.filter(lambda line: 'api_client' in line)
+most_http_requests_client = client_with_most_http_requests(api_client_lines)
+print(most_http_requests_client)
 
 most_failed_requests_client = client_with_most_failed_requests(log_rdd)
-print("client_with_most_requests:",failed_rdd[0])
-
+print(most_failed_requests_client)
 
 most_active_repo_count = count_most_active_repository(log_rdd)
-print("count of gh torrent-rb:", most_active_repo)
 print(most_active_repo_count)
+
 
 

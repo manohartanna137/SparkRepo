6fe99949bb7eb18d1efb5f1a9f0f3af1fd23186a
manohartanna137 <tanna.manohar@diggibyte.com>
2023-06-16 18:13:40 +0530

    This a test file for gh torrent logs text data

--- test/assignment_2/assignment_2_test.py
+++ test/assignment_2/assignment_2_test.py
@@ -1,36 +1,107 @@
 import unittest
 from pyspark import SparkContext
+
 from src.assignment_2.utils import *
 
 
+
 class Test(unittest.TestCase):
+    def test_assignment(self):
+        filepath = "../../resource/test_assignment2.txt"
+       # creating spark session
+        spark = spark_session()
 
-    def setUp(self):
-        self.file_path = "C:/Users/hp/PycharmProjects/SparkRepo/resource/ghtorrent-logs.txt"
-        self.log_rdd =  load_log_rdd(self.file_path)
-        self.api_client_lines = self.log_rdd.filter(lambda x: 'api_client' in x)
+        # creating rdd
 
-    def test_num_of_lines(self):
-        num_lines = num_of_lines(self.log_rdd)
-        self.assertEqual(num_lines, 281234)
-    def test_warning_messages(self):
-        warning_count = warning_messages(self.log_rdd)
-        self.assertEqual(warning_count, 3811)
+        rdd = creating_rdd(spark,filepath)
+        actual_rdd=rdd.collect()
+        expected_rdd=['DEBUG,2017-03-24T13:56:38+00:00,ghtorrent-46 -- ghtorrent.rb: Repo agco/harvesterjs exists', 'WARN,2017-03-23T11:04:43+00:00,ghtorrent-32 -- ght_data_retrieval.rb: Error processing event. Type: IssueCommentEvent, ID: 5531718847, Time: 64 ms', 'DEBUG,2017-03-23T10:44:44+00:00,ghtorrent-14 -- api_client.rb: Sleeping for 931 seconds', 'DEBUG,2017-03-23T09:36:14+00:00,ghtorrent-42 -- api_client.rb: Sleeping for 1441 seconds']
+        self.assertEqual(actual_rdd,expected_rdd)
 
-    def test_processed_repositories(self):
-        processed_count = process_repositories(self.log_rdd)
-        self.assertEqual(processed_count, 37596)
+        # test case for number of lines in rdd
 
-    def test_most_requests(self):
-        requests_client = most_requests(self.api_client_lines)
-        self.assertEqual(requests_client, "api_client.rb: Unauthorised request with token: 46f11b5791b7db9077f4d9a9ab27f93e89dccad4")
+        num_lines = num_of_lines(rdd)
+        expected_output=4
+        self.assertEqual(num_lines,expected_output)
 
-    def test_failed_requests(self):
-        most_failed_requests_client =failed_requests(self.log_rdd)
-        self.assertEqual(most_failed_requests_client,"api_client.rb: Failed request. URL: https://api.github.com/repos/greatfakeman/Tabchi/commits?sha=Tabchi&per_page=100, Status code: 404, Status: Not Found, Access: ac6168f8776, IP: 0.0.0.0, Remaining: 1749")
+        # test case for creating a dataframe from rdd
 
-    def test_active_repository(self):
-        active_repo_= active_repository(self.log_rdd)
-        self.assertEqual(active_repo_,166397)
+        df = reading_file(spark,filepath)
+        df_torrent = create_dataframe_from_rdd(df)
+        df_torrent.show(truncate=False)
+
+        data1 = [
+            ("DEBUG", "2017-03-24T13:56:38+00:00", "ghtorrent-46 ", " ghtorrent.rb", " Repo agco/harvesterjs exists"),
+            ("WARN", "2017-03-23T11:04:43+00:00", "ghtorrent-32 ", " ght_data_retrieval.rb",
+             " Error processing event. Type"),
+            ("DEBUG", "2017-03-23T10:44:44+00:00", "ghtorrent-14 ", " api_client.rb", " Sleeping for 931 seconds"),
+            ("DEBUG", "2017-03-23T09:36:14+00:00", "ghtorrent-42 ", " api_client.rb", " Sleeping for 1441 seconds")
+        ]
+
+        df_expec = spark.createDataFrame(data1,
+                                   ["log message", "timestamp", "downloader", "repository_clients", "commit_message"])
+
+        df_expec.show(truncate=False)
+        self.assertEqual(df_torrent.collect(),df_expec.collect())
+
+        # getting count of warn messages
+
+        df_warn = warn_messages(df_torrent)
+        expected_warn_output=1
+        self.assertEqual(df_warn,expected_warn_output)
+
+
+        # test case for getting failed clients
+
+
+        df_api_clients_actual = api_clients(df_torrent)
+        expected_api_clients=2
+        self.assertEqual(df_api_clients_actual,expected_api_clients)
+
+        # test case for most http requests
+
+        df_most_http_requests = most_http_requests(df_torrent)
+        df_most_http_requests.show()
+        schema_most_requests = StructType([
+            StructField("repository_clients", StringType())])
+        data_most_requests = []
+        expected_most_req_df=spark.createDataFrame(data=data_most_requests,schema=schema_most_requests)
+        self.assertEqual(df_most_http_requests.collect(),expected_most_req_df.collect())
+
+        # test case for most failed requests
+
+        df_most_failed_requests = most_failed_requests(df_torrent)
+        schema_most_failed = StructType([
+            StructField("repository_clients", StringType())])
+        data_most_failed = []
+        expected_most_failed_df = spark.createDataFrame(data=data_most_failed, schema=schema_most_failed)
+        self.assertEqual(df_most_failed_requests.collect(), expected_most_failed_df.collect())
+
+        # test case for most active hours
+
+
+        df_most_active_hours = most_active_hours(df_torrent)
+        expected_active_data=[(16,)]
+        expected_active_schema = StructType([
+            StructField('hour', IntegerType(), True)
+        ])
+        expected_df_more_active_hours=spark.createDataFrame(data=expected_active_data,schema=expected_active_schema)
+        self.assertEqual(df_most_active_hours.collect(),expected_df_more_active_hours.collect())
+
+        # test case for most active repositories
+
+        df_most_active_repositories = most_active_repositories(df_torrent)
+        expected_activerepo_schema = StructType([
+            StructField('log message', StringType(), True),
+            StructField('timestamp', StringType(), True),
+            StructField('downloader', StringType(), True),
+            StructField('repository_clients', StringType(), True),
+            StructField('commit_message', StringType(), True)
+        ])
+        expected_activerepo_data = [
+            ("DEBUG", "2017-03-24T13:56:38+00:00", "ghtorrent-46 ", " ghtorrent.rb", " Repo agco/harvesterjs exists")
+        ]
+        expected_activerepo_df=spark.createDataFrame(data=expected_activerepo_data,schema=expected_activerepo_schema)
+        self.assertEqual(df_most_active_repositories.collect(),expected_activerepo_df.collect())
 
 
